cbef9c2ab33e300e2ffacab9ac16e3e713303755
jader_tao <jader_tao@qq.com>
2017-08-17 14:57:26 +0800

    you can 'then' it now

--- fakeoath/fakeoath.ts
+++ fakeoath/fakeoath.ts
@@ -11,25 +11,31 @@ class Fakeoath {
   }
   static all() { }
   static race() { }
+
   public static toString(): string {
     return 'Fakeoath() { [native code] }'
   }
   public static valueOf(): string {
     return 'Fakeoath() { [native code] }'
   }
+
   constructor(arg: any) {
     this['[[status]]'] = 'pending'
-    this['[[value]]'] = arg
+
     switch (Object.prototype.toString.call(arg).toLowerCase().slice(8, -1)) {
       case 'function':
-        const resolve = v => { this['[[status]]'] = 'resolved', this['[[value]]'] = v }
-        const reject = v => { this['[[status]]'] = 'rejected', this['[[value]]'] = v }
-        arg(resolve, reject)
-        break
-      case 'number':
-      case 'string':
-        this['[[status]]'] = 'resolved'
-        this['[[value]]'] = arg
+        this['[[value]]'] = undefined
+        const resolve = v => {
+          this['[[status]]'] = 'resolved'
+          this['[[value]]'] = v
+          if (this.fullfilledCb) this.fullfilledCb()
+        }
+        const reject = v => {
+          this['[[status]]'] = 'rejected'
+          this['[[value]]'] = v
+          if (this.rejectedCb) this.rejectedCb()
+        }
+        this['[[value]]'] = arg(resolve, reject)
         break
       default:
         this['[[status]]'] = 'resolved'
@@ -38,13 +44,21 @@ class Fakeoath {
     }
   }
 
-  public then(onFullfilled: any, onRejected: any) {
-    return this['[[status]]'] === 'resolved'
-      ? new Fakeoath(onFullfilled(this['[[value]]']))
-      : new Fakeoath(onRejected(this['[[value]]']))
+  public then(f: any, r: any) {
+    let _oath = new Fakeoath((f, r) => { })
+    this.fullfilledCb = () => {
+      _oath['[[value]]'] = f()
+      _oath['[[value]]'] = 'resolved'
+    }
+    this.rejectedCb = () => {
+      _oath['[[value]]'] = r()
+      _oath['[[status]]'] = 'rejected'
+    }
+    return _oath
   }
+
   public catch() { }
 
-  private queue: any = { fullfilledQueue: [], rejectedQueue: [] }
+  private fullfilledCb
+  private rejectedCb
 }
-
